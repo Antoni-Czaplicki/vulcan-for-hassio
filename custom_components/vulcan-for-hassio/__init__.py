aec1ae452effd96c9567585027963d553cec9130
Czapla-dev <56671347+Czapla-dev@users.noreply.github.com>
2020-09-07 18:35:27 +0200

    Update __init__.py

--- custom_components/vulcan-for-hassio/__init__.py
+++ custom_components/vulcan-for-hassio/__init__.py
@@ -5,6 +5,7 @@ def reg(token, symbol, pin):
     certificate = Vulcan.register(token, symbol, pin)
     with open('cert.json', 'w') as f: 
         json.dump(certificate.json, f)
+       
         
 DOMAIN = "vulcan"
 
