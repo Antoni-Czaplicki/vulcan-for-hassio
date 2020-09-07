e3b60d78cdd877d73393fff6262dd66558b47538
Czapla-dev <56671347+Czapla-dev@users.noreply.github.com>
2020-09-07 13:26:58 +0200

    Create config_flow.py

---
+++ custom_components/vulcan-for-hassio/config_flow.py
@@ -0,0 +1,32 @@
+import logging
+
+import voluptuous as vol
+from homeassistant.config_entries import ConfigFlow
+
+from . import DOMAIN, vulcan
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class vulcanFlowHandler(ConfigFlow, domain=DOMAIN):
+    async def async_step_user(self, user_input=None):
+        """GUI > Configuration > Integrations > Plus > Uonet+ Vulcan for Home Assistant"""
+        error = None
+
+        if user_input is not None:
+            error = vulcan.is_vul(user_input['token'], user_input['symbol'], user_input['pin'])
+            if not error:
+                return self.async_create_entry(title='vulcan',
+                                               data=user_input['symbol'])
+
+        return self.async_show_form(
+            step_id='user',
+            data_schema=vol.Schema({
+                vol.Required('token'): str,
+                vol.Required('symbol'): str,
+                vol.Required('pin'): int,
+            }),
+            description_placeholders={
+                'error_text': "\nERROR: " + error if error else ''
+            }
+        )
