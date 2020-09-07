cf3b28aa5fbf044e81ea862bd9183de88f63efe8
Czapla-dev <56671347+Czapla-dev@users.noreply.github.com>
2020-09-07 22:08:24 +0200

    Update __init__.py

--- custom_components/vulcan-for-hassio/__init__.py
+++ custom_components/vulcan-for-hassio/__init__.py
@@ -1,5 +1,12 @@
 import json
 from vulcan import Vulcan
+import logging
+import voluptuous as vol
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import STATE_UNKNOWN
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.entity import Entity
        
         
 DOMAIN = "vulcan"
@@ -10,3 +17,15 @@ def setup(hass, config):
     # Return boolean to indicate that initialization was successful.
     return True
 
+#class Lessons(device):
+#    @property
+#    def device_info(self) -> Dict[str, Any]:
+#        return {
+#            "identifiers": {
+#                (DOMAIN, config_entry.symbol)
+#            },
+#            "name": "Lessons",
+#            "manufacturer": "Uonet+",
+#            "sw_version": "0.1.0",
+#            "entry_type": "service",
+#        }
