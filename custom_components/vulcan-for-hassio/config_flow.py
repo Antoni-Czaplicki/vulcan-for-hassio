73844f1f88ef52853f31adb6c0d3bb070ab4856f
Czapla-dev <56671347+Czapla-dev@users.noreply.github.com>
2020-09-07 17:44:20 +0200

    Update config_flow.py

--- custom_components/vulcan-for-hassio/config_flow.py
+++ custom_components/vulcan-for-hassio/config_flow.py
@@ -4,7 +4,7 @@ from vulcan import Vulcan
 import voluptuous as vol
 from homeassistant.config_entries import ConfigFlow
 
-from .const import DOMAIN
+from . import DOMAIN, register
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -13,21 +13,19 @@ class vulcanFlowHandler(ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         """GUI > Configuration > Integrations > Plus > Uonet+ Vulcan for Home Assistant"""
         error = None
-        
-        vulcan = 'test'
 
         if user_input is not None:
-            error = vulcan + user_input['token'] + user_input['symbol'] + str(user_input['pin'])
-            if not error:
-                return self.async_create_entry(title='vulcan',
-                                               data=user_input['symbol'])
+            return self.async_create_entry(title=user_input['symbol'],
+                                           data=user_input)
+            register.reg(user_input['token'], user_input['symbol'], user_input['pin'])
+        
 
         return self.async_show_form(
             step_id='user',
             data_schema=vol.Schema({
                 vol.Required('token'): str,
                 vol.Required('symbol'): str,
-                vol.Required('pin'): int,
+                vol.Required('pin'): str,
             }),
             description_placeholders={
                 'error_text': "\nERROR: " + error if error else ''
