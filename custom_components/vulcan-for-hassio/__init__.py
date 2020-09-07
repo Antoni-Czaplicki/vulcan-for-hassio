7b4e9716edc66e8c5a9da5ed2f6d0a0a699b3a44
Czapla-dev <56671347+Czapla-dev@users.noreply.github.com>
2020-09-07 18:12:18 +0200

    Update __init__.py

--- custom_components/vulcan-for-hassio/__init__.py
+++ custom_components/vulcan-for-hassio/__init__.py
@@ -1,9 +1,16 @@
+
+import json
+from vulcan import Vulcan
+def reg(token, symbol, pin):
+    certificate = Vulcan.register(token, symbol, pin)
+    with open('cert.json', 'w') as f: 
+        json.dump(certificate.json, f)
+        
 DOMAIN = "vulcan"
-"""
 
 def setup(hass, config):
     hass.states.set("vulcan.world", "Sing in successfully")
 
     # Return boolean to indicate that initialization was successful.
     return True
-"""
+
