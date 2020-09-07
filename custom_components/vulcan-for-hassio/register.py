1780b540234b59fa2494ff4c3626939f91f83075
Czapla-dev <56671347+Czapla-dev@users.noreply.github.com>
2020-09-07 17:43:44 +0200

    Update register.py

--- custom_components/vulcan-for-hassio/register.py
+++ custom_components/vulcan-for-hassio/register.py
@@ -1,4 +1,4 @@
-def register(token, symbol, pin)
+def reg(token, symbol, pin)
     certificate = Vulcan.register(token, symbol, pin)
     with open('cert.json', 'w') as f: 
         json.dump(certificate.json, f)
