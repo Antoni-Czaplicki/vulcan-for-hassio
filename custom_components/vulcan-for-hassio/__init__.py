c4f4db24525ae85d38ed4b2c51690c9d649939e2
Czapla-dev <56671347+Czapla-dev@users.noreply.github.com>
2020-09-08 22:00:05 +0200

    Update __init__.py

--- custom_components/vulcan-for-hassio/__init__.py
+++ custom_components/vulcan-for-hassio/__init__.py
@@ -5,16 +5,10 @@ from homeassistant.helpers import config_validation as cv, entity_platform, serv
  
 DOMAIN = "vulcan"
 
-#def setup_platform(hass, config, add_devices, discovery_info=None):
-#    """Set up the sensor platform."""
-#    add_entities([ExampleSensor()])
+
 
 def setup(hass, config):
-    """Your controller/hub specific code."""
-    # Data that you want to share with your platforms
-    hass.data[DOMAIN] = {
-        'temperature': 23
-    }
+
 
     hass.helpers.discovery.load_platform('sensor', DOMAIN, {}, config)
 
@@ -24,15 +18,4 @@ async def async_setup_entry(hass, entry):
     """Set up the media player platform for Sonos."""
 
     platform = entity_platform.current_platform.get()
-
-    # This will call Entity.set_sleep_timer(sleep_time=VALUE)
-#    platform.async_register_entity_service(
-#        SERVICE_UPDATE_DATA,
-#        {
-#            vol.Required('update_data'): str,
-#        },
-#        "update_data",
-#    )
-
-#self.hass.async_add_job(hass.config_entries.async_forward_entry_setup(
-#    self.config_entry, 'sensor'))
+    return True
