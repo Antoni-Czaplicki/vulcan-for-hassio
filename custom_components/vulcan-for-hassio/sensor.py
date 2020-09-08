275ff5f5996b9860ee5ad16efa008802bacbb179
Czapla-dev <56671347+Czapla-dev@users.noreply.github.com>
2020-09-08 15:41:41 +0200

    Update sensor.py

--- custom_components/vulcan-for-hassio/sensor.py
+++ custom_components/vulcan-for-hassio/sensor.py
@@ -1,14 +1,32 @@
 """Platform for sensor integration."""
 from homeassistant.const import TEMP_CELSIUS
 from homeassistant.helpers.entity import Entity
+import json
+from vulcan import Vulcan
+from homeassistant.helpers import config_validation as cv, entity_platform, service
+
+with open('vulcan.json') as f:
+    certificate = json.load(f)
+client = Vulcan(certificate)
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
-    """Set up the sensor platform."""
-    add_entities([ExampleSensor()])
+#    Set up the sensor platform.
+    if discovery_info is None:
+        return
+    add_entities([Lesson1()])
+    add_entities([Lesson2()])
+    add_entities([Lesson3()])
+    add_entities([Lesson4()])
+    add_entities([Lesson5()])
+    add_entities([Lesson6()])
+    add_entities([Lesson7()])
+    add_entities([Lesson8()])
+    add_entities([Lesson9()])
+    add_entities([Lesson10()])
+ 
 
-
-class ExampleSensor(Entity):
+class Lesson1(Entity):
     """Representation of a Sensor."""
 
     def __init__(self):
@@ -18,20 +36,241 @@ class ExampleSensor(Entity):
     @property
     def name(self):
         """Return the name of the sensor."""
-        return 'Example Temperature'
+        return 'Lesson 1'
+
+    @property
+    def device_state_attributes(self):
+        test = {
+            "room": "3",
+            "teacher": "E. K",
+            "time": "8.00-8.45"
+        }
+        return test
+    
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    def update(self):
+        """Fetch new state data for the sensor.
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self._state = 'lesson'
+
+
+class Lesson2(Entity):
+    """Representation of a Sensor."""
+
+    def __init__(self):
+        """Initialize the sensor."""
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Lesson 2'
 
     @property
     def state(self):
         """Return the state of the sensor."""
         return self._state
 
+    def update(self):
+        """Fetch new state data for the sensor.
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self._state = lesson
+
+
+class Lesson3(Entity):
+    """Representation of a Sensor."""
+
+    def __init__(self):
+        """Initialize the sensor."""
+        self._state = None
+
     @property
-    def unit_of_measurement(self):
-        """Return the unit of measurement."""
-        return TEMP_CELSIUS
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Lesson 3'
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
 
     def update(self):
         """Fetch new state data for the sensor.
         This is the only method that should fetch new data for Home Assistant.
         """
-        self._state = 23
+        self._state = lesson
+
+
+class Lesson4(Entity):
+    """Representation of a Sensor."""
+
+    def __init__(self):
+        """Initialize the sensor."""
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Lesson 4'
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    def update(self):
+        """Fetch new state data for the sensor.
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self._state = lesson
+
+
+class Lesson5(Entity):
+    """Representation of a Sensor."""
+
+    def __init__(self):
+        """Initialize the sensor."""
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Lesson 5'
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    def update(self):
+        """Fetch new state data for the sensor.
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self._state = lesson
+
+
+class Lesson6(Entity):
+    """Representation of a Sensor."""
+
+    def __init__(self):
+        """Initialize the sensor."""
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Lesson 6'
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    def update(self):
+        """Fetch new state data for the sensor.
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self._state = lesson
+
+
+
+class Lesson7(Entity):
+    """Representation of a Sensor."""
+
+    def __init__(self):
+        """Initialize the sensor."""
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Lesson 7'
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    def update(self):
+        """Fetch new state data for the sensor.
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self._state = lesson
+
+
+class Lesson8(Entity):
+    """Representation of a Sensor."""
+
+    def __init__(self):
+        """Initialize the sensor."""
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Lesson 8'
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    def update(self):
+        """Fetch new state data for the sensor.
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self._state = lesson
+
+
+class Lesson9(Entity):
+    """Representation of a Sensor."""
+
+    def __init__(self):
+        """Initialize the sensor."""
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Lesson 9'
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    def update(self):
+        """Fetch new state data for the sensor.
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self._state = lesson
+
+
+class Lesson10(Entity):
+    """Representation of a Sensor."""
+
+    def __init__(self):
+        """Initialize the sensor."""
+        self._state = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return 'Lesson 10'
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    def update(self):
+        """Fetch new state data for the sensor.
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        self._state = lesson
