686323f1dcffe1618abbb0e25b107abdb29e507d
Czapla-dev <56671347+Czapla-dev@users.noreply.github.com>
2020-09-11 22:37:28 +0200

    Update const.py

--- custom_components/vulcan-for-hassio/const.py
+++ custom_components/vulcan-for-hassio/const.py
@@ -1,6 +1,5 @@
-
 """Constants for the Vulcan integration."""
 
-# This is the internal name of the integration, it should also match the directory
-# name for the integration.
 DOMAIN = "vulcan"
+CONF_STUDENT_NAME = "student_name"
+CONF_GROUPS = "groups"
