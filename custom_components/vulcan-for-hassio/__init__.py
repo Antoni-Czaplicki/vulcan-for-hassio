27124b1ca7da0baca9beece8025e7219f26310f4
Czapla-dev <56671347+Czapla-dev@users.noreply.github.com>
2020-09-11 22:38:49 +0200

    Update __init__.py

--- custom_components/vulcan-for-hassio/__init__.py
+++ custom_components/vulcan-for-hassio/__init__.py
@@ -1,21 +1,42 @@
 import asyncio
 from homeassistant import config_entries
 import homeassistant
+import voluptuous as vol
 from homeassistant.helpers import config_validation as cv, entity_platform, service
- 
-DOMAIN = "vulcan"
+from .const import (  # pylint: disable=unused-import
+    CONF_STUDENT_NAME,
+    CONF_GROUPS,
+    DOMAIN,
+)
 
 
 
-def setup(hass, config):
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Optional(CONF_GROUPS): {
+            cv.string: vol.Schema({
+                vol.Optional('angielski'): cv.string,
+            }, extra=vol.ALLOW_EXTRA),
+        },
+        vol.Optional(CONF_STUDENT_NAME, default=''): cv.string,
+    }, extra=vol.ALLOW_EXTRA),
+}, extra=vol.ALLOW_EXTRA)
 
 
+async def async_setup(hass, config) -> bool:
+    vulcan: Optional[ConfigType] = config.get(DOMAIN)
+    hass.data.setdefault(DOMAIN, {})
+
+    if not vulcan:
+        return True
+    
+    hass.data[DOMAIN] = {
+        'student_name': config[DOMAIN][CONF_STUDENT_NAME],
+    }
+    
     hass.helpers.discovery.load_platform('sensor', DOMAIN, {}, config)
-
     return True
 
 async def async_setup_entry(hass, entry):
-    """Set up the media player platform for Sonos."""
-
     platform = entity_platform.current_platform.get()
     return True
