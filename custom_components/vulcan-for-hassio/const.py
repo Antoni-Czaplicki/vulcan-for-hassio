72c32b1c76875b9d922ecdf1e30df9d7d2d0c01c
Czapla-dev <56671347+Czapla-dev@users.noreply.github.com>
2020-09-17 14:36:06 +0200

    Update const.py

--- custom_components/vulcan-for-hassio/const.py
+++ custom_components/vulcan-for-hassio/const.py
@@ -1,5 +1,7 @@
 """Constants for the Vulcan integration."""
+from datetime import timedelta
 
+SCAN_INTERVAL = timedelta(seconds=300)
 DOMAIN = "vulcan"
 CONF_STUDENT_NAME = "student_name"
 CONF_GROUPS = "groups"
