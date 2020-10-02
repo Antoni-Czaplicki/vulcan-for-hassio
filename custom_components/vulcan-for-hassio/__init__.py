dfafa5b7b1ca12a48756fe2de22ba3de698b0ce1
Czapla-dev <56671347+Czapla-dev@users.noreply.github.com>
2020-10-02 10:44:05 +0200

    Update __init__.py

--- custom_components/vulcan-for-hassio/__init__.py
+++ custom_components/vulcan-for-hassio/__init__.py
@@ -2,19 +2,46 @@
 import asyncio
 from homeassistant import config_entries
 import homeassistant
+from homeassistant.components import persistent_notification
 import json
 from vulcan import Vulcan
 import voluptuous as vol
+import os
+import logging
 from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers import config_validation as cv, entity_platform, service
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
 from .const import ( 
     CONF_STUDENT_NAME,
     CONF_GROUPS,
     DOMAIN,
+    CONF_NOTIFY,
 )
-with open('vulcan.json') as f:
-    certificate = json.load(f)
-client = Vulcan(certificate)
+
+def get_students_list():
+    with open('vulcan.json') as f:
+        certificate = json.load(f)
+    client = Vulcan(certificate)
+    students_list = {'0': '* Default'}
+    for student in client.get_students():
+        students_list[str(student.id)] = student.name
+    return students_list
+
+_LOGGER = logging.getLogger(__name__)
+autherror=False
+try:
+    if os.stat("vulcan.json").st_size == 0:
+        autherror=True
+    else:
+        with open('vulcan.json') as f:
+            certificate = json.load(f)
+        client = Vulcan(certificate)
+except FileNotFoundError:
+    autherror=True
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
@@ -30,6 +57,10 @@ CONFIG_SCHEMA = vol.Schema({
 async def async_setup(hass, config) -> bool:
     vulcan: Optional[ConfigType] = config.get(DOMAIN)
     hass.data.setdefault(DOMAIN, {})
+    
+    if autherror == True:
+        persistent_notification.async_create(hass, 'Coudl not import certificate, try to reconfigure integration', "Vulcan: Authentication error")
+        
     if not vulcan:
         return True
     for student in client.get_students():
@@ -38,13 +69,21 @@ async def async_setup(hass, config) -> bool:
             break
 
     hass.data[DOMAIN] = {
-        'student_name': config[DOMAIN][CONF_STUDENT_NAME],
+        'student_name_old': config[DOMAIN][CONF_STUDENT_NAME],
         'groups': config[DOMAIN][CONF_GROUPS],
     }
     
     hass.helpers.discovery.load_platform('sensor', DOMAIN, {}, config)
     return True
 
-async def async_setup_entry(hass, entry):
+async def async_setup_entry(hass, config_entry):
+    students_list = get_students_list()
+    hass.data[DOMAIN]['notify'] = config_entry.options.get(CONF_NOTIFY)
+    hass.data[DOMAIN]['student_name'] = students_list[config_entry.options.get(CONF_STUDENT_NAME)]
+
+    for student in client.get_students():
+        if student.name == students_list[config_entry.options.get(CONF_STUDENT_NAME)]:
+            client.set_student(student)
+            break
     platform = entity_platform.current_platform.get()
     return True
