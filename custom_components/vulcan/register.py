5163ab131117a66d731c6f093524b4e603759cfe
Czapla <56671347+Czapla-dev@users.noreply.github.com>
2020-10-22 23:30:04 +0200

    Update register.py

--- custom_components/vulcan/register.py
+++ custom_components/vulcan/register.py
@@ -1,9 +1,10 @@
 import json
 from vulcan import Vulcan
-def reg(token, symbol, pin):
-    certificate = Vulcan.register(token, symbol, pin)
-    with open('vulcan.json', 'w') as f: 
-        json.dump(certificate.json, f)
-        
-    return None
 
+
+def reg(token, symbol, pin):
+    certificate = Vulcan.register(token, symbol, pin, "Home Assistant")
+    with open("vulcan.json", "w") as f:
+        json.dump(certificate.json, f)
+
+    return None
