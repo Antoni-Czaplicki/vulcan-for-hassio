ee683289a4986f5d1b379051d1c9dc060eca9f97
Czapla <56671347+Antoni-Czaplicki@users.noreply.github.com>
2020-11-30 08:38:42 +0100

    Update const.py

--- custom_components/vulcan/const.py
+++ custom_components/vulcan/const.py
@@ -1,5 +1,7 @@
 """Constants for the Vulcan integration."""
 from datetime import timedelta
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
 
 
 DOMAIN = "vulcan"
@@ -8,3 +10,11 @@ CONF_GROUPS = "groups"
 CONF_NOTIFY = "notify"
 CONF_ATTENDANCE_NOTIFY = "attendance_notify"
 CONF_SCAN_INTERVAL = timedelta(minutes=1)
+
+SEND_MESSAGE_SERVICE_SCHEMA = vol.Schema(
+    {
+        vol.Required('teacher'): cv.positive_int,
+        vol.Required('title'): cv.string,
+        vol.Optional('content'): cv.string,
+    }
+)
