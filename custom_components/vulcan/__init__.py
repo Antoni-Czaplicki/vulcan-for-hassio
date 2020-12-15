cf0bb5de68f2d5da94324243da7718e8203e9d54
Czapla <56671347+Antoni-Czaplicki@users.noreply.github.com>
2020-12-15 17:51:07 +0100

    Update __init__.py

--- custom_components/vulcan/__init__.py
+++ custom_components/vulcan/__init__.py
@@ -9,6 +9,8 @@ import voluptuous as vol
 import os
 import logging
 from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers.typing import ConfigType, HomeAssistantType
+from homeassistant.helpers.entity import Entity
 from homeassistant.helpers import config_validation as cv, entity_platform, service
 from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity,
@@ -34,23 +36,33 @@ def get_students_list():
     return students_list
 
 
-_LOGGER = logging.getLogger(__name__)
-autherror = False
 try:
     if os.stat("vulcan.json").st_size == 0:
         autherror = True
     else:
+        autherror = False
         with open("vulcan.json") as f:
             certificate = json.load(f)
         client = Vulcan(certificate)
 except FileNotFoundError:
     autherror = True
+except StopIteration:
+    if os.path.exists("vulcan.json"):
+        os.remove("vulcan.json")
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_will_remove_from_hass(self) -> None:
+    await super().async_will_remove_from_hass()
+    if os.path.exists("vulcan.json"):
+        os.remove("vulcan.json")
 
 
 async def async_setup(hass, config) -> bool:
     vulcan: Optional[ConfigType] = config.get(DOMAIN)
     hass.data.setdefault(DOMAIN, {})
-
     if autherror == True:
         persistent_notification.async_create(
             hass,
@@ -61,7 +73,6 @@ async def async_setup(hass, config) -> bool:
     if not vulcan:
         return True
 
-    hass.helpers.discovery.load_platform("sensor", DOMAIN, {}, config)
     return True
 
 
@@ -69,6 +80,7 @@ async def async_setup_entry(hass, config_entry):
     students_list = get_students_list()
     hass.data[DOMAIN]["notify"] = config_entry.options.get(CONF_NOTIFY)
     hass.data[DOMAIN]["att_notify"] = config_entry.options.get(CONF_ATTENDANCE_NOTIFY)
+
     try:
         hass.data[DOMAIN]["student_name"] = students_list[
             config_entry.options.get(CONF_STUDENT_NAME)
@@ -79,6 +91,8 @@ async def async_setup_entry(hass, config_entry):
         if student.name == hass.data[DOMAIN]["student_name"]:
             client.set_student(student)
             break
-    platform = entity_platform.current_platform.get()
-    hass.helpers.discovery.load_platform("sensor", DOMAIN, {}, config_entry)
+
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+    )
     return True
