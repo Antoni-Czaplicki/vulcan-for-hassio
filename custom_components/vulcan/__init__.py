4ebd578cda5088a150c7096543ea222de4bf3d3c
Antoni-Czaplicki <antekczaplicki@gmail.com>
2021-01-31 16:19:30 +0100

    Migrate to hebe

--- custom_components/vulcan/__init__.py
+++ custom_components/vulcan/__init__.py
@@ -1,96 +1,67 @@
 # This Integration uses unofficial Vulcan-api https://github.com/kapi2289/vulcan-api
-import asyncio
-from homeassistant import config_entries
-import homeassistant
-from homeassistant.components import persistent_notification
-import json
-from vulcan import Vulcan
-import voluptuous as vol
-import os
 import logging
-from homeassistant.helpers import device_registry as dr
-from homeassistant.helpers.typing import ConfigType, HomeAssistantType
+
+from vulcan import Account, Keystore, VulcanHebe
+
 from homeassistant.helpers.entity import Entity
-from homeassistant.helpers import config_validation as cv, entity_platform, service
-from homeassistant.helpers.update_coordinator import (
-    CoordinatorEntity,
-    DataUpdateCoordinator,
-    UpdateFailed,
-)
-from .const import (
-    CONF_STUDENT_NAME,
-    CONF_GROUPS,
-    DOMAIN,
-    CONF_NOTIFY,
-    CONF_ATTENDANCE_NOTIFY,
-)
+from homeassistant.helpers.typing import ConfigType
 
-
-try:
-    if os.stat("vulcan.json").st_size == 0:
-        autherror = True
-    else:
-        autherror = False
-        with open("vulcan.json") as f:
-            certificate = json.load(f)
-        client = Vulcan(certificate)
-except FileNotFoundError:
-    autherror = True
-except StopIteration:
-    if os.path.exists("vulcan.json"):
-        os.remove("vulcan.json")
-
-
-def get_students_list():
-    students_list = {}
-    for student in client.get_students():
-        students_list[str(student.id)] = student.name
-    return students_list
-
+from .const import CONF_ATTENDANCE_NOTIFY, CONF_NOTIFY, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
-
-
-async def async_will_remove_from_hass(self) -> None:
-    await super().async_will_remove_from_hass()
-    if os.path.exists("vulcan.json"):
-        os.remove("vulcan.json")
 
 
 async def async_setup(hass, config) -> bool:
     vulcan: Optional[ConfigType] = config.get(DOMAIN)
     hass.data.setdefault(DOMAIN, {})
-    if autherror == True:
-        persistent_notification.async_create(
-            hass,
-            "Coudl not import certificate, try to reconfigure integration",
-            "Vulcan: Authentication error",
-        )
-
-    if not vulcan:
-        return True
 
     return True
 
 
 async def async_setup_entry(hass, config_entry):
-    students_list = get_students_list()
     hass.data[DOMAIN]["notify"] = config_entry.options.get(CONF_NOTIFY)
     hass.data[DOMAIN]["att_notify"] = config_entry.options.get(CONF_ATTENDANCE_NOTIFY)
-
+    autherror = False
     try:
-        hass.data[DOMAIN]["student_name"] = students_list[
-            config_entry.options.get(CONF_STUDENT_NAME)
-        ]
-    except KeyError:
-        hass.data[DOMAIN]["student_name"] = "default"
+        with open(".vulcan/keystore.json") as f:
+            keystore = Keystore.load(f)
+        with open(".vulcan/account.json") as f:
+            account = Account.load(f)
+        client = VulcanHebe(keystore, account)
+        students = await client.get_students()
+        for student in students:
+            if student.pupil.id == config_entry.data.get("student_id"):
+                client.student = student
+                break
+        await client.close()
+    except:
+        autherror = True
 
-    for student in client.get_students():
-        if student.name == hass.data[DOMAIN]["student_name"]:
-            client.set_student(student)
-            break
+    if autherror == True:
+        hass.async_create_task(
+            hass.config_entries.flow.async_init(DOMAIN, context={"source": "reauth"})
+        )
+        return False
 
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
     )
     return True
+
+
+class VulcanEntity(Entity):
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def icon(self):
+        return self._icon
+
+    @property
+    def unique_id(self):
+        return self._unique_id
+
+    @property
+    def state(self):
+        return self._state
--- custom_components/vulcan/register.py
+++ custom_components/vulcan/register.py
@@ -1,5 +1,9 @@
 import json
-from vulcan import Vulcan
+from pathlib import Path
+
+from vulcan import Account, Keystore, Vulcan, VulcanHebe
+
+from . import DOMAIN
 
 
 def reg(token, symbol, pin):
@@ -8,3 +12,13 @@ def reg(token, symbol, pin):
         json.dump(certificate.json, f)
 
     return None
+
+
+async def register(token, symbol, pin):
+    Path(".vulcan").mkdir(parents=True, exist_ok=True)
+    keystore = Keystore.create(device_model="Home Assistant")
+    with open(".vulcan/keystore.json", "w") as f:
+        f.write(keystore.as_json)
+    account = await Account.register(keystore, token, symbol, pin)
+    with open(".vulcan/account.json", "w") as f:
+        f.write(account.as_json)
