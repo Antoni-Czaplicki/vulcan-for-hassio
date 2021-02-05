a6fc1cdeef6bb7d4c8b5ae6c136bb2b259728129
Czapla <56671347+Antoni-Czaplicki@users.noreply.github.com>
2021-02-05 15:36:23 +0100

    Update reauth

--- custom_components/vulcan/config_flow.py
+++ custom_components/vulcan/config_flow.py
@@ -200,7 +200,7 @@ class vulcanFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
                     config_data["student_id"] = str(student.pupil.id)
                     config_data["students_number"] = students_number
                     self.hass.config_entries.async_update_entry(
-                        existing_entry, data=config_data
+                        existing_entry, title=f"{self._student.pupil.first_name} {self._student.pupil.last_name}" ,data=config_data
                     )
                     await self.hass.config_entries.async_reload(existing_entry.entry_id)
                 return self.async_abort(reason="reauth_successful")
