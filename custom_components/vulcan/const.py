73fa96ff2b3ac575caca8ab5b501152c6a009ba2
Antoni Czaplicki <antekczaplicki@gmail.com>
2021-04-13 13:04:13 +0200

    remove unused variables

--- custom_components/vulcan/const.py
+++ custom_components/vulcan/const.py
@@ -1,21 +1,8 @@
 """Constants for the Vulcan integration."""
-from datetime import timedelta
-
-import homeassistant.helpers.config_validation as cv
-import voluptuous as vol
 
 DOMAIN = "vulcan"
 CONF_MESSAGE_NOTIFY = "message_notify"
 CONF_GRADE_NOTIFY = "grade_notify"
 CONF_ATTENDANCE_NOTIFY = "attendance_notify"
-SCAN_INTERVAL = timedelta(minutes=1)
 DEFAULT_SCAN_INTERVAL = 5
 PARALLEL_UPDATES = 1
-
-SEND_MESSAGE_SERVICE_SCHEMA = vol.Schema(
-    {
-        vol.Required("teacher"): cv.positive_int,
-        vol.Required("title"): cv.string,
-        vol.Optional("content"): cv.string,
-    }
-)
