846c16a99d283285ea1699e801bddb64766710df
tiwek <74269691+tiwek@users.noreply.github.com>
2022-03-24 10:02:57 +0100

    Update calendar.py

--- custom_components/vulcan/calendar.py
+++ custom_components/vulcan/calendar.py
@@ -158,7 +158,7 @@ class VulcanCalendarData:
                         item["date"], item["time"].to
                     ).strftime(DATE_STR_FORMAT)
                 },
-                "summary": item["lesson"] if item["lesson"] else "" ,
+                "summary": item["lesson"] if item["lesson"] else "",
                 "location": item["room"],
                 "description": item["teacher"],
             }
